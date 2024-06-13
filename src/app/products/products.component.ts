56c0f797ae9d4e338d1a70131c30c02f3a48218a
R-ELKADAH <rachidelkadah@email.com>
2024-06-13 10:57:45 +0100

    work with db.json and checked

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,18 +1,42 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrl: './products.component.css'
 })
-export class ProductsComponent {
+export class ProductsComponent implements OnInit{
   products :Array<any> =[
-    {id : 1, name : "Computer", price : 4500, checked : false},
-    {id : 2, name : "Printer", price : 4500, checked : true},
-    {id : 3, name : "Phone", price : 4500, checked : false}
-  ]
+    // {id : 1, name : "Computer", price : 4500, checked : false},
+    // {id : 2, name : "Printer", price : 4500, checked : true},
+    // {id : 3, name : "Phone", price : 4500, checked : false}
+  ];
+  constructor(private http:HttpClient) {
+  }
+  ngOnInit() {
+   this.getProducts();
+  }
+
+  getProducts(){
+    this.http.get<Array<any>>("http://localhost:8089/products")
+      .subscribe({
+        next : data =>{
+          this.products=data
+        },
+        error : err =>{}
+      })
+  }
+
+
 
   handleCheckProduct(product: any) {
-    product.checked=!product.checked;
-  }
+    this.http.patch<any>(`http://localhost:8089/products/${product.id}`,
+      {checked:!product.checked}).subscribe({
+      next : updateProduct => {
+        product.checked=!product.checked;
+        // this.getProducts();
+          }
+        })
+      }
 }
