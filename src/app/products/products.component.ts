7486bec6288f0e484c422c4beb0c63645e1939a9
R-ELKADAH <rachidelkadah@email.com>
2024-06-13 10:42:09 +0100

    work without db.json

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrl: './products.component.css'
 })
 export class ProductsComponent {
+  products :Array<any> =[
+    {id : 1, name : "Computer", price : 4500, checked : false},
+    {id : 2, name : "Printer", price : 4500, checked : true},
+    {id : 3, name : "Phone", price : 4500, checked : false}
+  ]
 
+  handleCheckProduct(product: any) {
+    product.checked=!product.checked;
+  }
 }
