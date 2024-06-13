8b45bf1104aea4ba86f6f0a023ddb02ca0c864fe
R-ELKADAH <rachidelkadah@email.com>
2024-06-13 11:05:19 +0100

    work with db.json and checked and use services (observbale)

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
+import {ProductService} from "../services/product.service";
 
 @Component({
   selector: 'app-products',
@@ -12,15 +13,14 @@ export class ProductsComponent implements OnInit{
     // {id : 2, name : "Printer", price : 4500, checked : true},
     // {id : 3, name : "Phone", price : 4500, checked : false}
   ];
-  constructor(private http:HttpClient) {
+  constructor(private productService:ProductService) {
   }
   ngOnInit() {
    this.getProducts();
   }
 
   getProducts(){
-    this.http.get<Array<any>>("http://localhost:8089/products")
-      .subscribe({
+    this.productService.getProducts().subscribe({
         next : data =>{
           this.products=data
         },
@@ -31,8 +31,7 @@ export class ProductsComponent implements OnInit{
 
 
   handleCheckProduct(product: any) {
-    this.http.patch<any>(`http://localhost:8089/products/${product.id}`,
-      {checked:!product.checked}).subscribe({
+    this.productService.checkProduct(product).subscribe({
       next : updateProduct => {
         product.checked=!product.checked;
         // this.getProducts();
